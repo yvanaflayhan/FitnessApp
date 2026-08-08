bcc889aa9fd6a51f96c95a87033db6462cfe3cde
yvana flayhan <flayhanyvana1@gmail.com>
2026-08-08 20:29:22 +0300

    Add Angular routing and auth guard

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    })
   ]
 };
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { GymLists } from './gyms/gym-lists/gym-lists';
+import { GymDetail } from './gyms/gym-detail/gym-detail';
+import { Trainers } from './trainers/trainers';
+import { Bookings } from './bookings/bookings';
+import { Profile } from './profile/profile';
+import { authGuard } from './_guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'gyms', component: GymLists },
+            { path: 'gyms/:id', component: GymDetail },
+            { path: 'trainers', component: Trainers },
+            { path: 'bookings', component: Bookings },
+            { path: 'profile', component: Profile },
+
+        ]
+    },
+    { path: '**', component: Home, pathMatch: 'full' },
+
+];
+
