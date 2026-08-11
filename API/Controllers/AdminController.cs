664010f878c1e0fd39bbd5c6ea795b92a0313b82
yvana flayhan <flayhanyvana1@gmail.com>
2026-08-11 15:02:17 +0300

    Add admin user retrieval endpoints

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,3 +1,5 @@
+using API.DTOs;
+using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,10 +8,35 @@ namespace API.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : BaseApiController
     {
+        private readonly IUserService _userService;
+
+        public AdminController (IUserService userService)
+        {
+            _userService = userService;
+        }
         [HttpGet("test")]
         public IActionResult Test()
         {
             return Ok("You are an Admin!");
+        }
+
+        [HttpGet("users")]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
+            var users = await _userService.GetUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("users/{id}")]
+        public async Task<ActionResult<MemberDto>> GetUser(int id)
+        {
+            var user = await _userService.GetUserAsync(id);
+
+            if(user == null)
+                return NotFound();
+
+            return Ok(user);
+            
         }
         
     }
