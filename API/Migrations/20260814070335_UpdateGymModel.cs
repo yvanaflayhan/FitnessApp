3bac47e4cbfadae2933f83758fa7170749cb6ab2
yvana flayhan <flayhanyvana1@gmail.com>
2026-08-14 10:49:11 +0300

    Add gym database migration

---
+++ API/Migrations/20260814070335_UpdateGymModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGymModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Gym",
+                table: "Gym");
+
+            migrationBuilder.RenameTable(
+                name: "Gym",
+                newName: "Gyms");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Gyms",
+                table: "Gyms",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Gyms",
+                table: "Gyms");
+
+            migrationBuilder.RenameTable(
+                name: "Gyms",
+                newName: "Gym");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Gym",
+                table: "Gym",
+                column: "Id");
+        }
+    }
+}
