7344beee11dbb33096f888ea63a66231ed5fa10c
yvana flayhan <flayhanyvana1@gmail.com>
2026-08-20 11:00:15 +0300

    Update gym model

--- client/src/app/_models/gym.ts
+++ client/src/app/_models/gym.ts
@@ -1,8 +1,14 @@
-export interface Gym
-{
+export interface Gym {
     id: number;
     name: string;
     address: string;
+    location: string;
     latitude: number;
     longitude: number;
+    phone?: string;
+    openingHours?: string;
+    closingHours?: string;
+    description?: string;
+    imageUrl?: string;
+    socialMedia?: string;
 }
