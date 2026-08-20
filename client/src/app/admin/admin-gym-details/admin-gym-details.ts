5cfd1c1cdeb18098d689c5a4d3baf46f7b8f7f32
yvana flayhan <flayhanyvana1@gmail.com>
2026-08-20 13:04:06 +0300

    created gym details component

---
+++ client/src/app/admin/admin-gym-details/admin-gym-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-gym-details',
+  imports: [],
+  templateUrl: './admin-gym-details.html',
+  styleUrl: './admin-gym-details.css',
+})
+export class AdminGymDetails {}
